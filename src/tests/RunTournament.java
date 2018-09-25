c3a13ef1e0c82f5c0747755711e1d18e5e6d1505
Raluca D. Gaina <r.d.gaina@qmul.ac.uk>
2018-09-25 12:00:28 +0100

    correct defaults

--- src/tests/RunTournament.java
+++ src/tests/RunTournament.java
@@ -17,18 +17,18 @@ public class RunTournament {
     public static void main(String args[]) throws Exception {
 
         // Set tournament settings
-        int rounds = 2;                             // Number of rounds in the tournament
-        int timeBudget = 100;                       // Time budget allowed per action (default 100ms)
-        int maxGameLength = 2000;                   // Maximum game length (default 2000 ticks)
-        boolean fullObservability = true;           // Full or partial observability (default true)
-        boolean selfMatches = false;                // If self-play should be used (default false)
-        boolean timeOutCheck = true;                // If the game should count as a loss if a bot times out (default true)
-        boolean preAnalysis = false;                // If bots are allowed to analyse the game before starting (default true)
-        int preAnalysisBudgetFirstTimeInAMap = 0;   // Time budget for pre-analysis if playing first time on a new map (default 0)
-        int preAnalysisBudgetRestOfTimes = 0;       // Time budget for pre-analysis for all other cases (default 0)
-        boolean runGC = false;                      // If Java Garbage Collector should be called before each player action (default false)
-        int iterationBudget = -1;                   // Iteration budget, set to -1 for infinite (default: -1)
-        int playOnlyWithThisAI = -1;                //  AI index in list of AIs, if one AI should be included in all matches played (default -1)
+        int rounds = 2;                                // Number of rounds in the tournament
+        int timeBudget = 100;                          // Time budget allowed per action (default 100ms)
+        int maxGameLength = 2000;                      // Maximum game length (default 2000 ticks)
+        boolean fullObservability = true;              // Full or partial observability (default true)
+        boolean selfMatches = false;                   // If self-play should be used (default false)
+        boolean timeOutCheck = true;                   // If the game should count as a loss if a bot times out (default true)
+        boolean preAnalysis = true;                    // If bots are allowed to analyse the game before starting (default true)
+        int preAnalysisBudgetFirstTimeInAMap = 1000;   // Time budget for pre-analysis if playing first time on a new map (default 1s)
+        int preAnalysisBudgetRestOfTimes = 1000;       // Time budget for pre-analysis for all other cases (default 1s)
+        boolean runGC = false;                         // If Java Garbage Collector should be called before each player action (default false)
+        int iterationBudget = -1;                      // Iteration budget, set to -1 for infinite (default: -1)
+        int playOnlyWithThisAI = -1;                   //  AI index in list of AIs, if one AI should be included in all matches played (default -1)
 
         // Create list of AIs participating in tournament
         List<AI> AIs = new ArrayList<>();
